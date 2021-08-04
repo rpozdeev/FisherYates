db80424ab91aa25e875075423f534b681afe01c1
Roman I Pozdeev <rip@m2.ru>
2021-08-04 16:38:22 +0300

    add

--- Package.swift
+++ Package.swift
@@ -6,23 +6,15 @@ import PackageDescription
 let package = Package(
     name: "FisherYates",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "FisherYates",
             targets: ["FisherYates"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FisherYates",
-            dependencies: []),
-        .testTarget(
-            name: "FisherYatesTests",
-            dependencies: ["FisherYates"]),
+            dependencies: []), 
     ]
 )
--- Sources/FisherYates/FisherYates.swift
+++ Sources/FisherYates/FisherYates.swift
@@ -1,3 +1,21 @@
-struct FisherYates {
-    var text = "Hello, World!"
+public extension Collection {
+    func shuffled() -> [Iterator.Element] {
+        var array = Array(self)
+        array.shuffle()
+        return array
+    }
+}
+
+public extension MutableCollection {
+    mutating func shuffle() {
+        var i = startIndex
+        var n = count
+        
+        while n > 1 {
+            let j = index(i, offsetBy: random(n))
+            swapAt(i, j)
+            formIndex(after: &i)
+            n -= 1
+        }
+    }
 }
